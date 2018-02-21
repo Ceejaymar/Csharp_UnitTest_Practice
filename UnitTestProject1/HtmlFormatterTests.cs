ac9c76e2f343c1ac387fc529bec495ed1dfcbfcc
carlos <v-camart@microsoft.com>
2018-02-20 23:37:25 -0800

    Add unit test for formatasbold

---
+++ UnitTestProject1/HtmlFormatterTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+	[TestFixture]
+	public class HtmlFormatterTests
+	{
+		[Test]
+		public void FormatAsBold_WhenCalled_ShoulEncloseTheStringWithStrongElement()
+		{
+			var formatter = new HtmlFormatter();
+
+			var result = formatter.FormatAsBold("abc");
+
+			//Specfic assertion
+			Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase); //assertions area case sensitive by default
+
+			//More general aassertion
+			Assert.That(result, Does.StartWith("<strong>"));
+			Assert.That(result, Does.EndWith("</strong>"));
+			Assert.That(result, Does.Contain("abc"));
+		}
+	}
+}
